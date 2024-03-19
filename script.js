4a6e418f9d073dca99708f695f0a7f74d2362fc8
Vedika Mishra <151787416+vedikamishragithub@users.noreply.github.com>
2024-03-19 15:38:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 function volume_sphere() {
-    //Write your code here
-  
-} 
-
+    var calRadius = parseFloat(document.getElementById("radius").value);         
+    var result = 'NaN';
+    if (!isNaN(calRadius) && calRadius >= 0) {
+        var result = (4/3) * Math.PI * Math.pow(calRadius, 3);
+        result = result.toFixed(4);
+    }
+    var calVolume = document.getElementById("volume"); 
+    calVolume.value = result;  
+}
+window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
 window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
